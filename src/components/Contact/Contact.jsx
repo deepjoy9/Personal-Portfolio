306262f45129bb8a06065c3e2fd0eee2fdfdba83
deepjoy9 <deepjoysarkar9@gmail.com>
2024-03-10 21:18:03 +0530

    Added Contact Section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">deepjoysarkar9@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/deepjoy9">
+            linkedin.com/deepjoy9
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/deepjoy9">github.com/deepjoy9</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
